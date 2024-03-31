2860a2631fd1d22b1711d5933bb884564cac8d17
AyAyRon BaLaKey <intmax47@gmail.com>
2024-03-31 22:26:34 +0300

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,29 @@ app = Flask(
 )
 
 @app.route( "/" )
+@app.route( "/index.html" )
 def home():
     return render_template( "index.html" )
+
+@app.route( "/world-one" )
+@app.route( "/world-one.html" )
+def worldOne():
+    return render_template( "world-one.html" )
+
+@app.route( "/world-two" )
+@app.route( "/world-two.html" )
+def worldTwo():
+    return render_template( "world-two.html" )
+
+@app.route( "/world-three" )
+@app.route( "/world-three.html" )
+def worldThree():
+    return render_template( "world-three.html" )
+
+@app.route( "/space" )
+@app.route( "/space.html" )
+def space():
+    return render_template( "space.html" )
 
 if __name__ == "__main__":
     app.run( debug = True )
